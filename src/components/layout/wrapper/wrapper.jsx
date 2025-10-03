d1364cb0148be80037450cded58787f1e6d1466b
Fuad <fuadalizade626@gmail.com>
2025-10-03 17:09:01 +0400

    123

--- src/components/layout/wrapper/wrapper.jsx
+++ src/components/layout/wrapper/wrapper.jsx
@@ -1,12 +1,12 @@
-import Header from '../header/Header.jsx'
-import Footer from '../footer/footer.jsx'
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
 
 export default function Wrapper({ children }) {
-    return (
-        <div className={`flex flex-col min-h-svh`}>
-            <Header />
-            <main className={`flex-1`}>{children}</main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className={`flex flex-col min-h-svh`}>
+      <Header />
+      <main className={`flex-1`}>{children}</main>
+      <Footer />
+    </div>
+  );
 }
