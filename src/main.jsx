7e5928c2b9a1141b0f9090eaf5eadfd7954a7a0f
Fuad <fuadalizade626@gmail.com>
2025-10-04 11:57:48 +0400

    fuuuu

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import Wrapper from "./components/layout/wrapper/wrapper.jsx";
 import "../src/components/layout/shared/styles/index.css";
-import Hero from "./components/sections/Hero.jsx";
+import Hero from "./components/sections/hero.jsx";
 
 createRoot(document.getElementById("root")).render(
   <Wrapper>
